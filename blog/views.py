a79d56a3759ad2e74d819c2f64df90b4db52a79b
Abell112233 <abelllfineghan@gmail.com>
2024-08-29 09:45:06 -0300

    ultima alteração

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,27 @@ from django.shortcuts import render
 from .models import Eu, Filmes, Series
 # Create your views here.
 def index(request):
-    filmes = Filmes.objects.all()
-    series = Series.objects.all()
+    filme = Filmes.objects.all()
+    serie = Series.objects.all()
     contexto = {
-        "filmes" : filmes,
-        "series" : series
+        'filmes' : filme,
+        'series' : serie
     }
     
     return render(request, 'blog/index.html', contexto)
+
+def SobreFilme(request, filmes_id):
+    detalhe = Filmes.objects.get(id=filmes_id)
+    contexto = {
+        'detalhe' : detalhe
+    }
+    
+    return render(request, 'blog/sobrefilme.html', contexto)
+
+def SobreSerie(request, serie_id):
+    detalhe2 = Series.objects.get(id=serie_id)
+    contexto = {
+        'detalhe2' : detalhe2,
+    }
+    
+    return render(request, 'blog/sobreserie.html', contexto)
